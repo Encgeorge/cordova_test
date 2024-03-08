7008b3f47efd59aeb48e2f3d95151d7d26a62b3c
Encgeorge <49530826+Encgeorge@users.noreply.github.com>
2024-03-08 16:30:52 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -2,6 +2,7 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
     document.getElementById('captureButton').addEventListener('click', capturePhoto);
+    checkConnection();
 }
 
 function capturePhoto() {
@@ -27,6 +28,7 @@ function onPhotoSuccess(imageURI) {
 
     // Get the current location
     getCurrentLocation();
+   
 }
 
 function onPhotoFail(message) {
@@ -56,4 +58,20 @@ function onLocationSuccess(position) {
 
 function onLocationError(error) {
     alert('Failed to get location: ' + error.message);
+}
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN] = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI] = 'WiFi connection';
+    states[Connection.CELL_2G] = 'Cell 2G connection';
+    states[Connection.CELL_3G] = 'Cell 3G connection';
+    states[Connection.CELL_4G] = 'Cell 4G connection';
+    states[Connection.CELL] = 'Cell generic connection';
+    states[Connection.NONE] = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
 }
