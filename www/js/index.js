875600646d5a39d2d08e79322f97abb5b472c78d
Encgeorge <49530826+Encgeorge@users.noreply.github.com>
2024-03-08 09:58:23 +0200

    Add files via upload

---
+++ www/js/index.js
@@ -0,0 +1,31 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    document.getElementById('captureButton').addEventListener('click', capturePhoto);
+}
+
+function capturePhoto() {
+    var options = {
+        quality: 80,
+        destinationType: Camera.DestinationType.FILE_URI,
+        encodingType: Camera.EncodingType.JPEG,
+        mediaType: Camera.MediaType.PICTURE,
+        correctOrientation: true
+    };
+
+    navigator.camera.getPicture(onPhotoSuccess, onPhotoFail, options);
+}
+
+function onPhotoSuccess(imageURI) {
+    var photoContainer = document.getElementById('photoContainer');
+    var imgElement = document.createElement('img');
+    imgElement.src = imageURI;
+    imgElement.style.maxWidth = '100%';
+    photoContainer.innerHTML = '';
+    photoContainer.appendChild(imgElement);
+}
+
+function onPhotoFail(message) {
+    alert('Failed because: ' + message);
+}
+
